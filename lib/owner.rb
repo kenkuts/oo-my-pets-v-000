09e3d65d6f181141c79cde7704600b9010b70fde
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-28 06:48:52 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,8 +51,7 @@ class Owner
   end
 
   def list_pets
-    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s),
-     and #{pets[:cats].size} cat(s)."
+    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
   end
 
   def say_species
